2a188780ce6a1cb41a522bb8d9aa0782fd563161
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-04-16 17:13:44 -0300

    We create a login mutation and return a token

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -25,4 +25,26 @@ const register = {
     }
 };
 
-module.exports = {register}
+const login = {
+    type: GraphQLString,
+    args: {
+        email: {type: GraphQLString},
+        password: {type: GraphQLString}
+    },
+    async resolve(_, args){
+        
+        const user = await User.findOne({email: args.email}).select('+password')
+
+        if(!user || args.password !== user.password)
+            throw new Error('Invalid Credentials'); 
+
+        const token = createJWTToken({_id : user._id, username: user.username, email: user.email});
+
+        return token;
+    }
+}
+
+module.exports = {
+    register,
+    login
+}
